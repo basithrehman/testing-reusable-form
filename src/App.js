d5af6ea7d7a6324041ee89dea66f1ca940a56de3
Abdul Rahiman Basith <abdulrahimanbasith@gmail.com>
2021-05-18 17:14:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import { Button, Input } from 'aknamed';
+import { Button, Input, SingleSelect } from 'aknamed';
+
+const options = [
+  { value: 'option-1', label: 'option-1' },
+  { value: 'option-2', label: 'option-2' },
+  { value: 'option-3', label: 'option-3' }
+]
 
 function App() {
 return (
-  <div middle center column className="page" gap={1.5}>
-   <h3>This is my new project</h3>
-   <Button label="test" kind="primary"/>
-   <Input
-  label="Input"
-  primary
-  required
-  type="number"
-/>
+  <div style={{margin:'auto', width: '40%', marginTop: '150px'}}>
+   <h3 style={{textAlign: 'center', color: '#0c4040', marginBottom: '30px'}}>Aknamed Assignment</h3>
+
+  <Input
+    label="Input"
+    primary
+    required
+    type="email"
+    placeholder="Enter email ..."
+  />
+
+  <Input
+    label="Input"
+    primary
+    required
+    type="number"
+    placeholder="Enter number ..."
+  />
+
+  <SingleSelect options={options}></SingleSelect>
+
+  <div style={{ marginLeft: '45px', marginRight: '45px', marginTop: '20px' }}>
+  <Button label="Submit" kind="primary"/>
+  </div>
   </div>
 );
 }
